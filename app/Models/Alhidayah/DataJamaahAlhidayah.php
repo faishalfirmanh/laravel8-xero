ade7b411962b4d6f46e5ff91cc33f32837ebca01
Faishal Firman Hakim <isalredarmy1@gmail.com>
2026-06-26 10:01:29 +0700

    detail alamat excel

---
+++ app/Models/Alhidayah/DataJamaahAlhidayah.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Alhidayah;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DataJamaahAlhidayah extends Model
+{
+    use HasFactory;
+}
